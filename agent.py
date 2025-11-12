15668e5b261b8cdbe038e3e4a04537a30075f69c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2025-11-12 15:08:03 +0000

    バックエンドとしてファイルシステムを使うよう変更

--- agent.py
+++ agent.py
@@ -1,6 +1,9 @@
 from deepagents import create_deep_agent
+from deepagents.backends import FilesystemBackend
 from langgraph.graph.state import CompiledStateGraph
 
 
 def create_my_agent() -> CompiledStateGraph:
-    return create_deep_agent()
+    return create_deep_agent(
+        backend=FilesystemBackend(root_dir=".", virtual_mode=True),
+    )
